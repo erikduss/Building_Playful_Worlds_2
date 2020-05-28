04624fef85ec2e5685632da362892371eb8245b6
Erikduss <erik.dusschooten@student.hku.nl>
2020-05-28 10:13:40 +0200

    Added project

---
+++ BPW_2/Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float playerSpeed = 2f;
+    private Rigidbody2D rbPlayer;
+    private Animator animPlayer;
+
+	// Use this for initialization
+	void Start () {
+        rbPlayer = this.GetComponent<Rigidbody2D>();
+        animPlayer = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        MovePlayer();     
+	}
+
+    void MovePlayer()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        /*Vector3 inputVector = new Vector3(hor, 0, ver);
+        inputVector.Normalize();*/
+
+        rbPlayer.velocity = new Vector2(hor * playerSpeed, ver * playerSpeed);
+
+        animPlayer.SetFloat("horizontalSpeed", rbPlayer.velocity.x);
+        animPlayer.SetFloat("verticalSpeed", rbPlayer.velocity.y);
+
+        Debug.Log(rbPlayer.velocity);
+    }
+
+}
