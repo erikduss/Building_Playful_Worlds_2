1c2428711bd93419802803ded9613540452c73f1
Erikduss <erik.dusschooten@student.hku.nl>
2020-06-19 23:08:04 +0200

    Added player attacks

--- BPW_2/Assets/PlayerController.cs
+++ BPW_2/Assets/PlayerController.cs
@@ -10,31 +10,77 @@ public class PlayerController : MonoBehaviour {
 
     private string facingDirection = "Front";
 
+    private bool freezePlayer = false;
+
 	// Use this for initialization
 	void Start () {
         rbPlayer = this.GetComponent<Rigidbody2D>();
         animPlayer = this.GetComponent<Animator>();
 	}
 	
+    void getDirection()
+    {
+        Debug.Log(facingDirection);
+    }
+
 	// Update is called once per frame
 	void FixedUpdate () {
-        MovePlayer();
+        if (!freezePlayer)
+        {
+            MovePlayer();
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            rbPlayer.velocity = new Vector2(0,0);
+            animPlayer.SetFloat("horizontalSpeed", 0);
+            animPlayer.SetFloat("verticalSpeed", 0);
 
+            switch (facingDirection)
+            {
+                case "Front":
+                    animPlayer.Play("front_slashing");
+                    break;
+                case "Back":
+                    animPlayer.Play("back_slashing");
+                    break;
+                case "Left":
+                    animPlayer.Play("left_slashing");
+                    break;
+                case "Right":
+                    animPlayer.Play("right_slashing");
+                    break;
+            }
+            StartCoroutine(freezePlayerMovement(1));
         }
 
 	}
+
+    private IEnumerator freezePlayerMovement(int time)
+    {
+        freezePlayer = true;
+        yield return new WaitForSeconds(time);
+        freezePlayer = false;
+    }
 
     void MovePlayer()
     {
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
-        if(Mathf.Abs(hor) > Mathf.Abs(ver))
+        rbPlayer.velocity = new Vector2(hor * playerSpeed, ver * playerSpeed);
+
+        animPlayer.SetFloat("horizontalSpeed", hor);
+        animPlayer.SetFloat("verticalSpeed", ver);
+
+        if(hor == 0 && ver == 0)
         {
-            if(hor < 0)
+            return;
+        }
+
+        if (Mathf.Abs(hor) > Mathf.Abs(ver))
+        {
+            if (hor < 0)
             {
                 facingDirection = "Left";
                 animPlayer.SetInteger("Direction", 4);
@@ -58,13 +104,6 @@ public class PlayerController : MonoBehaviour {
                 animPlayer.SetInteger("Direction", 3);
             }
         }
-
-
-
-        rbPlayer.velocity = new Vector2(hor * playerSpeed, ver * playerSpeed);
-
-        animPlayer.SetFloat("horizontalSpeed", hor);
-        animPlayer.SetFloat("verticalSpeed", ver);
     }
 
 }
